9fdd63feb9d1012bb778a94ce2a205a52993e174
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-12-07 16:19:44 -0400

    Se realizo una configuracion personalizada de los factoris con archivos json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Project;
+use App\Models\Skill;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        Skill::factory(6)->create();
+        Project::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
